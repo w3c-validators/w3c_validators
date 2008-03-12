41c98224bf93e8db7b7d1cd27f5c0dd1964ba99b
Alex Dunae <alex@dunae.ca>
2008-03-12 02:47:14 +0000

    Prepared 0.9.3 gem

--- rakefile.rb
+++ rakefile.rb
@@ -30,7 +30,7 @@ end
 
 spec = Gem::Specification.new do |s| 
   s.name = "w3c_validators"
-  s.version = "0.9.2"
+  s.version = "0.9.3"
   s.author = "Alex Dunae"
   s.homepage = "http://code.dunae.ca/w3c_validators"
   s.platform = Gem::Platform::RUBY

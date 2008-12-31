6f4e1887055543a5321e03e629a2fe12ba3ed31d
Alex Dunae <alex@dunae.ca>
2008-12-31 09:55:55 -0800

    Updated for github

--- rakefile.rb
+++ rakefile.rb
@@ -1,7 +1,6 @@
 require 'rake'
 require 'rake/testtask'
 require 'rake/rdoctask'
-require 'rake/gempackagetask'
 require 'lib/w3c_validators'
 require 'fileutils'
 
@@ -15,39 +14,41 @@ Rake::TestTask.new do |t|
   t.verbose = false
 end
 
-
 desc 'Generate documentation for W3C Validators.'
 Rake::RDocTask.new(:rdoc) do |rdoc|
   rdoc.rdoc_dir = 'doc'
   rdoc.title    = 'Ruby W3C Validators'
   rdoc.options << '--all' << '--inline-source' << '--line-numbers'
-  rdoc.rdoc_files.include('README')
+  rdoc.rdoc_files.include('README.rdoc')
   rdoc.rdoc_files.include('LICENSE')
   rdoc.rdoc_files.include('CHANGELOG')
   rdoc.rdoc_files.include('lib/*.rb')
   rdoc.rdoc_files.include('lib/w3c_validators/*.rb')
 end
 
-
-spec = Gem::Specification.new do |s| 
-  s.name = "w3c_validators"
-  s.version = "0.9.3"
-  s.author = "Alex Dunae"
-  s.homepage = "http://code.dunae.ca/w3c_validators"
+desc 'Create the gem spec'
+task :create_gemspec do
+  File.open("w3c-validators.gemspec","w") do |file|
+    file.puts <<-EOH
+# Auto-generated gemspec
+# Run 'rake generate_gemspec' to re-generate
+Gem::Specification.new do |s|
+  s.name     = "w3c_validators"
   s.platform = Gem::Platform::RUBY
-  s.description = <<-EOF
-    W3C Validators is a Ruby wrapper for the World Wide Web Consortium's online validation services.
-  EOF
-  s.summary = "Wrapper for the World Wide Web Consortium's online validation services."
-  s.files = FileList["{lib}/**/*"].to_a
-  s.test_files = Dir.glob('test/test_*.rb') 
+  s.version  = "1.0.0"
+  s.date     = "2008-12-31"
+  s.summary  = "Wrapper for the World Wide Web Consortium's online validation services."
+  s.email    = "code@dunae.ca"
+  s.homepage = "http://code.dunae.ca/w3c_validators"
+  s.description = "W3C Validators is a Ruby wrapper for the World Wide Web Consortium's online validation services."
   s.has_rdoc = true
-  s.extra_rdoc_files = ["README", "LICENSE", "CHANGELOG"]
-  s.rdoc_options << '--all' << '--inline-source' << '--line-numbers'
+  s.author  = "Alex Dunae"
+  s.extra_rdoc_files = ['README.rdoc', 'CHANGELOG', 'LICENSE']
+  s.rdoc_options << '--all' << '--inline-source' << '--line-numbers' << '--charset' << 'utf-8'
+EOH
+  
+    file.puts "  s.test_files = ['" +  Dir.glob('test/test_*.rb').join("','") + "']"
+    file.puts "  s.files = ['" +  FileList["{lib}/**/*"].join("','") + "']"
+    file.puts 'end'
+  end
 end
-
-desc 'Build the W3C Validators gem.'
-Rake::GemPackageTask.new(spec) do |pkg| 
-  pkg.need_zip = true
-  pkg.need_tar = true 
-end 
